3a3cf546cc79a94234efeb84b5299a1f47eef4ea
NickAlger <nalger225@gmail.com>
2021-10-13 18:22:40 -0500

    revert to previous point-in-node tree implementation for speed

--- src/mesh_operations.h
+++ src/mesh_operations.h
@@ -49,23 +49,25 @@ class KDTree {
 private:
     typedef Matrix<double, K, 1> KDVector;
 
+    // Node in KD tree
+    struct Node { KDVector point;
+                  int      left;       // index of left child
+                  int      right; };   // index of right child
+
     // Nearest neighbor in subtree to query point
     struct SubtreeResult { int index; // index of nearest neighbor
                            double distance_squared; }; // distance squared to nearest neighbor
 
-    VectorXi                   mids;                 // indices of splitting (mid) nodes
-    VectorXi                   lefts;                // indices of children left of each node
-    VectorXi                   rights;               // indices of children right of each node
-    Matrix<double, K, Dynamic> ordered_points_array; // All points in the tree, depth-first ordered. column major
+    vector< Node > nodes; // All nodes in the tree
 
     // creates subtree and returns the index for root of subtree
     int make_subtree( int start, int stop, int depth,
                       vector< KDVector > & points,
                       int & counter ) {
         int num_pts_local = stop - start;
-        int current = -1; // -1 indicates node does not exist
+        int current_node_ind = -1; // -1 indicates node does not exist
         if (num_pts_local >= 1) {
-            current = counter;
+            current_node_ind = counter;
             counter = counter + 1;
 
             int axis = depth % K;
@@ -73,18 +75,28 @@ private:
                   [axis](KDVector u, KDVector v) {return u(axis) > v(axis);} );
 
             int mid = start + (num_pts_local / 2);
-            mids(current) = mid;
-            lefts(current)  = make_subtree(start,    mid, depth + 1, points, counter);
-            rights(current) = make_subtree(mid + 1, stop, depth + 1, points, counter); }
-        return current; }
+
+            int left_start = start;
+            int left_stop = mid;
+
+            int right_start = mid + 1;
+            int right_stop = stop;
+
+            int left = make_subtree(left_start, left_stop, depth + 1, points, counter);
+            int right = make_subtree(right_start, right_stop, depth + 1, points, counter);
+
+            nodes[current_node_ind] = Node { points[mid], left, right }; }
+        return current_node_ind; }
 
     // finds nearest neighbor of query in subtree
     SubtreeResult nn_subtree( const KDVector & query,
-                              int              current,
+                              int              root_index,
                               int              depth) {
-        KDVector delta = query - ordered_points_array.col(mids(current));
+        Node root = nodes[root_index];
 
-        int best = current;
+        KDVector delta = query - root.point;
+
+        int best_index = root_index;
         double best_distance_squared = delta.squaredNorm();
 
         int axis = depth % K;
@@ -93,53 +105,45 @@ private:
         int A;
         int B;
         if (displacement_to_splitting_plane >= 0) {
-            A = lefts(current);
-            B = rights(current);
+            A = root.left;
+            B = root.right;
         } else {
-            A = rights(current);
-            B = lefts(current); }
+            A = root.right;
+            B = root.left; }
 
         if (A >= 0) {
             SubtreeResult nn_A = nn_subtree( query, A, depth + 1);
             if (nn_A.distance_squared < best_distance_squared) {
-                best = nn_A.index;
+                best_index = nn_A.index;
                 best_distance_squared = nn_A.distance_squared; } }
 
         if (B >= 0) {
-            bool nn_might_be_in_B_subtree =
+            bool nearest_neighbor_might_be_in_B_subtree =
                 displacement_to_splitting_plane*displacement_to_splitting_plane < best_distance_squared;
-            if (nn_might_be_in_B_subtree) {
+            if (nearest_neighbor_might_be_in_B_subtree) {
                 SubtreeResult nn_B = nn_subtree( query, B, depth + 1);
                 if (nn_B.distance_squared < best_distance_squared) {
-                    best = nn_B.index;
+                    best_index = nn_B.index;
                     best_distance_squared = nn_B.distance_squared; } } }
 
-        return SubtreeResult { best, best_distance_squared }; }
+        return SubtreeResult { best_index, best_distance_squared }; }
 
 public:
     KDTree( Array<double, Dynamic, K> & points_array ) {
         int num_pts = points_array.rows();
 
-        // Copy eigen matrix input into std::vector of points which will be re-ordered
+        // Copy eigen matrix input into std::vector of tuples which will be re-ordered
         vector< KDVector > points(num_pts);
         for ( int ii=0; ii<num_pts; ++ii) {
             points[ii] = points_array.row(ii); }
 
-        // Make tree
-        mids.resize(num_pts);
-        lefts.resize(num_pts);
-        rights.resize(num_pts);
+        nodes.reserve(num_pts);
         int counter = 0;
-        int zero = make_subtree(0, num_pts, 0, points, counter);
-
-        // Copy ordered vector of points into eigen matrix
-        ordered_points_array.resize(K, num_pts);
-        for ( int ii=0; ii<num_pts; ++ii) {
-            ordered_points_array.col(ii) = points[ii]; } }
+        int zero = make_subtree(0, num_pts, 0, points, counter); }
 
     pair<KDVector, double> nearest_neighbor( KDVector point ) {
         SubtreeResult nn_result = nn_subtree( point, 0, 0 );
-        return make_pair(ordered_points_array.col(mids(nn_result.index)),
+        return make_pair(nodes[nn_result.index].point,
                          nn_result.distance_squared); }
 
     pair< Array<double, Dynamic, K>, VectorXd >
@@ -154,7 +158,7 @@ public:
         for ( int ii=0; ii<num_querys; ++ii ) {
             KDVector query = query_array.row(ii);
             SubtreeResult nn_result = nn_subtree( query, 0, 0 );
-            closest_points_array.row(ii) = ordered_points_array.col(mids(nn_result.index));
+            closest_points_array.row(ii) = nodes[nn_result.index].point;
             squared_distances(ii) = nn_result.distance_squared; }
 
         return make_pair(closest_points_array, squared_distances); } };
@@ -167,7 +171,7 @@ import dolfin as dl
 import hlibpro_python_wrapper as hpro
 hcpp = hpro.hpro_cpp
 
-K = 4
+K = 2
 
 def make_KDT(pp):
     dim = pp.shape[1]
