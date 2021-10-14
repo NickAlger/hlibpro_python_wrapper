5e745e18d365e65e965c0e363daee51293714b17
NickAlger <nalger225@gmail.com>
2021-10-14 17:05:50 -0500

    finding points takes a lot more memory than expected

--- examples/test_aabbtree.py
+++ examples/test_aabbtree.py
@@ -51,3 +51,49 @@ points_that_are_in_at_least_one_box = np.any(np.all(np.logical_and(b_mins[None, 
 points_that_are_not_in_any_box = np.logical_not(points_that_are_in_at_least_one_box)
 bad_points_are_the_points_outside_all_boxes = np.all(points_that_are_not_in_any_box == bad_points)
 print('bad_points_are_the_points_outside_all_boxes=', bad_points_are_the_points_outside_all_boxes)
+
+#
+
+num_boxes = int(1e3)
+num_pts = int(1e6)
+
+print('num_boxes=', num_boxes, ', num_pts=', num_pts)
+
+box_area = 1. / num_boxes
+box_h = np.power(box_area, 1./K)
+
+box_centers = np.random.randn(num_boxes, K)
+box_widths = box_h * np.abs(np.random.randn(num_boxes, K))
+b_mins = box_centers - box_widths
+b_maxes = box_centers + box_widths
+
+qq = np.random.randn(num_pts, K)
+
+t = time()
+AABB = hcpp.AABBTree2D(b_mins, b_maxes)
+dt_build = time() - t
+print('dt_build=', dt_build)
+
+t = time()
+box_inds = AABB.first_point_intersection_vectorized(qq)
+dt_first_point_intersection_vectorized = time() - t
+print('dt_first_point_intersection_vectorized=', dt_first_point_intersection_vectorized)
+
+
+good_points = (box_inds >= 0)
+bad_points = np.logical_not(good_points)
+
+good_qq = qq[good_points,:]
+S1 = b_mins[box_inds[good_points], :] <= good_qq
+S2 = good_qq <= b_maxes[box_inds[good_points], :]
+good_points_that_are_in_their_box = np.logical_and(S1, S2)
+all_good_points_are_in_their_boxes = np.all(good_points_that_are_in_their_box)
+print('all_good_points_are_in_their_boxes=', all_good_points_are_in_their_boxes)
+
+
+points_that_are_in_at_least_one_box = np.any(np.all(np.logical_and(b_mins[None, :, :] <= qq[:, None, :],
+                                                                   qq[:, None, :] <= b_maxes[None, :, :]), axis=2), axis=1)
+
+points_that_are_not_in_any_box = np.logical_not(points_that_are_in_at_least_one_box)
+bad_points_are_the_points_outside_all_boxes = np.all(points_that_are_not_in_any_box == bad_points)
+print('bad_points_are_the_points_outside_all_boxes=', bad_points_are_the_points_outside_all_boxes)
