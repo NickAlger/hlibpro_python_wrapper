ea06e54cfd1e8324d95548d29bfc97b131628d85
NickAlger <nalger225@gmail.com>
2021-11-07 17:30:25 -0600

    evaluating block of PC kernel parallelizes well. about 6x speedup on 8 cores

--- src/product_convolution_kernel.h
+++ src/product_convolution_kernel.h
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <Eigen/Dense>
 
+#include "thread-pool-master/thread_pool.hpp"
+
 #include "kdtree.h"
 #include "misc.h"
 #include "rbf_interpolation.h"
@@ -32,6 +34,8 @@ private:
     double      tau_squared;
     double      rbf_sigma_squared;
     int         num_nearest_neighbors;
+
+    thread_pool pool;
 
 public:
     ProductConvolutionKernelRBF( const vector<Matrix<double, K, 1>> all_sample_points,
@@ -83,8 +87,8 @@ public:
 
     }
 
-    vector<pair<int, double>> interpolation_inds_and_values(const Matrix<double, K, 1> & y,
-                                                            const Matrix<double, K, 1> & x)
+    vector<pair<Matrix<double,K,1>, double>> interpolation_points_and_values(const Matrix<double, K, 1> & y,
+                                                                             const Matrix<double, K, 1> & x)
     {
         pair<VectorXi, VectorXd> nn_result = sample_points_kdtree.nearest_neighbors( x, num_nearest_neighbors );
         VectorXi nearest_inds = nn_result.first;
@@ -101,15 +105,8 @@ public:
             ind_is_good[jj] = ( all_IC[jj].simplex_ind >= 0 ); // y-x+xi is in mesh => varphi_i(y-x) is defined
         }
 
-        vector<pair<int, double>> good_inds_and_values;
-        good_inds_and_values.reserve(ind_is_good.size());
-
-        vector<int> good_inds;
-        good_inds.reserve(ind_is_good.size());
-
-        vector<double> ff;
-        ff.reserve(ind_is_good.size());
-
+        vector<pair<Matrix<double,K,1>, double>> good_points_and_values;
+        good_points_and_values.reserve(ind_is_good.size());
         for ( int jj=0; jj<N_nearest; ++jj )
         {
             if ( ind_is_good[jj] )
@@ -129,53 +126,36 @@ public:
                         varphi_at_y_minus_x += IC.affine_coords(kk) * phi_j(mesh.cells(kk, IC.simplex_ind));
                     }
                 }
-                good_inds_and_values.push_back(make_pair(ind, varphi_at_y_minus_x));
+                good_points_and_values.push_back(make_pair(SP.point - x, varphi_at_y_minus_x));
             }
         }
-
-        return good_inds_and_values;
+        return good_points_and_values;
     }
 
     double eval_integral_kernel(const Matrix<double, K, 1> & y, const Matrix<double, K, 1> & x)
     {
-        vector<pair<int, double>> inds_and_values   = interpolation_inds_and_values(y, x); // forward
-        vector<pair<int, double>> inds_and_values_T = interpolation_inds_and_values(x, y); // transpose (swap x, y)
-
-        vector<pair<Matrix<double,K,1>, double>> points_and_values; // forward
-        points_and_values.reserve(inds_and_values.size() + inds_and_values_T.size());
-        for ( pair<int, double> ind_and_value : inds_and_values )
-        {
-            int ind = ind_and_value.first;
-            double val = ind_and_value.second;
-            Matrix<double, K, 1> shifted_point = sample_points[ind].point - x;
-            points_and_values.push_back(make_pair(shifted_point, val));
-        }
-
-        vector<pair<Matrix<double,K,1>, double>> points_and_values_T; // transpose
-        points_and_values_T.reserve(inds_and_values_T.size());
-        for ( pair<int, double> ind_and_value_T : inds_and_values_T )
-        {
-            int ind = ind_and_value_T.first;
-            double val = ind_and_value_T.second;
-            Matrix<double, K, 1> shifted_point = sample_points[ind].point - y;
-            points_and_values_T.push_back(make_pair(shifted_point, val));
-        }
+        vector<pair<Matrix<double,K,1>, double>> points_and_values   = interpolation_points_and_values(y, x); // forward
+        vector<pair<Matrix<double,K,1>, double>> points_and_values_T = interpolation_points_and_values(x, y); // transpose (swap x, y)
 
         // Add non-duplicates. Inefficient implementation but whatever.
         // Asymptotic complexity not affected because we already have to do O(k^2) matrix operation later anyways
-        double tol = 1e-6;
+        double tol = 1e-8;
         double tol_squared = tol*tol;
 
-        for ( int ii=0; ii<points_and_values_T.size(); ++ii ) // inefficient, but whatever.
+        int N = points_and_values.size();
+        int N_T = points_and_values_T.size();
+        points_and_values.reserve( N + N_T );
+        for ( int ii=0; ii<N_T; ++ii ) // inefficient, but whatever.
         {
             pair<Matrix<double,K,1>, double> & PV_T = points_and_values_T[ii];
             bool is_duplicate = false;
-            for ( int jj=0; jj<points_and_values.size(); ++jj )
+            for ( int jj=0; jj<N; ++jj )
             {
                 pair<Matrix<double,K,1>, double> & PV = points_and_values[jj];
                 if ( (PV.first - PV_T.first).squaredNorm() < tol_squared )
                 {
                     is_duplicate = true;
+                    PV.second = 0.5*(PV.second + PV_T.second);
                     break;
                 }
             }
@@ -185,13 +165,13 @@ public:
             }
         }
 
-        int N_good = points_and_values.size();
+        int N_combined = points_and_values.size();
         double kernel_value = 0.0;
-        if ( N_good > 0 )
+        if ( N_combined > 0 )
         {
-            MatrixXd P(K, N_good);
-            VectorXd F(N_good);
-            for ( int jj=0; jj<N_good; ++jj )
+            MatrixXd P(K, N_combined);
+            VectorXd F(N_combined);
+            for ( int jj=0; jj<N_combined; ++jj )
             {
                 P.col(jj) = points_and_values[jj].first;
                 F(jj)     = points_and_values[jj].second;
@@ -207,13 +187,28 @@ public:
         int nx = xx.cols();
         int ny = yy.cols();
         MatrixXd block(ny, nx);
-        for ( int jj=0; jj<nx; ++jj )
+
+        auto loop = [&](const int &a, const int &b)
         {
-            for ( int ii=0; ii<ny; ++ii )
+            for ( int ind=a; ind<b; ++ind )
             {
+                int jj = ind / ny;
+                int ii = ind % ny;
                 block(ii, jj) = eval_integral_kernel(yy.col(ii), xx.col(jj));
             }
-        }
+        };
+
+        pool.parallelize_loop(0, nx * ny, loop);
+
+//        for ( int jj=0; jj<nx; ++jj )
+//        {
+//            for ( int ii=0; ii<ny; ++ii )
+//            {
+//                block(ii, jj) = eval_integral_kernel(yy.col(ii), xx.col(jj));
+//            }
+//        }
         return block;
     }
 };
+
+
