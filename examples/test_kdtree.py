f240944db295cc9493585adb7dd03547b30c3777
NickAlger <nalger225@gmail.com>
2021-11-14 18:35:29 -0600

    some segfault when using block size of 5

--- examples/test_kdtree.py
+++ examples/test_kdtree.py
@@ -39,11 +39,11 @@ print('')
 #
 
 print('timing:')
-K = 3
-n_pts = int(1e6)
+K = 2
+n_pts = int(1e3)
 n_query = int(1e6)
 num_neighbors = 10
-block_size = 32
+block_size = 5
 print('K=', K, 'n_pts=', n_pts, ', n_query=', n_query, ', num_neighbors=', num_neighbors, ', block_size=', block_size)
 
 pp = np.random.randn(n_pts, K)
