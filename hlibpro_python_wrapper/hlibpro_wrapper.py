fixed mu scaling problem in deflated shifted hmatrix adjuster

hlibpro_python_wrapper/hlibpro_wrapper.py
@@ -1309,13 +1309,21 @@ def make_shifted_hmatrix_inverse_interpolator(
         assert (mu > 0.0)
 
     # Get new mus to fill in gaps
-    if np.min(np.abs(np.array(known_mus) - mu_min)) > boundary_mu_rtol * mu_min:
+    dont_have_mu_min = True
+    if len(known_mus) > 0:
+        dont_have_mu_min = np.min(np.abs(np.array(known_mus) - mu_min)) > boundary_mu_rtol * mu_min
+
+    dont_have_mu_max = True
+    if len(known_mus) > 0:
+        dont_have_mu_max = np.min(np.abs(np.array(known_mus) - mu_max)) > boundary_mu_rtol * mu_max
+
+    if dont_have_mu_min:
         if display:
             print('making shifted factorization A + mu_min*B, mu_min=', mu_min)
         fac = make_shifted_factorization(A, B, mu_min, rtol=rtol, display=display)
         known_mus.append(mu_min)
         known_shifted_factorizations.append(fac)
-    if np.min(np.abs(np.array(known_mus) - mu_max)) > boundary_mu_rtol * mu_max:
+    if dont_have_mu_max:
         if display:
             print('making shifted factorization A + mu_max*B, mu_max=', mu_max)
         fac = make_shifted_factorization(A, B, mu_max, rtol=rtol, display=display)
@@ -1379,14 +1387,16 @@ def deflate_negative_eigs_then_make_shifted_hmatrix_inverse_interpolator(
     dd_min, V_min, shifts_min, factorized_shifted_matrices, LM_eig_min = negative_eigenvalues_of_hmatrix_pencil(
         A, B_min,
         save_intermediate_factorizations=save_intermediate_factorizations,
-        threshold=threshold, gamma=gamma, sigma_factor=np.sqrt(mu_spacing_factor), chunk_size=chunk_size,
+        threshold=threshold, sigma_factor=np.sqrt(mu_spacing_factor), chunk_size=chunk_size,
         tol=rtol, ncv_factor=ncv_factor, lanczos_maxiter=lanczos_maxiter, display=display,
         shifted_preconditioner_only=shifted_preconditioner_only)
+    # dd_min, V_min, shifts_min, factorized_shifted_matrices, LM_eig_min = negative_eigenvalues_of_hmatrix_pencil(
+    #     A, B_min, threshold=threshold)
 
-    V = V_min * np.sqrt(mu_min)
+    V = V_min / np.sqrt(mu_min) #* np.sqrt(mu_min)
     dd = dd_min * mu_min
     LM_eig = LM_eig_min * mu_min
-    known_mus = [-mu_min * shift for shift in shifts_min]
+    known_mus = [-shift * mu_min for shift in shifts_min]
     if display:
         print('known_mus=', known_mus)
 

hlibpro_python_wrapper/test_deflate_eigenvalues_shifted_solver.py
@@ -22,6 +22,9 @@ B_hmatrix = hpro.build_hmatrix_from_scipy_sparse_matrix(B_csc, bct)
 iA_hmatrix = hpro.build_hmatrix_from_scipy_sparse_matrix(iA_csc, bct)
 A_hmatrix = iA_hmatrix.inv()
 
+
+########    hpro.negative_eigenvalues_of_hmatrix_pencil    ########
+
 threshold = -0.25
 
 dd, V, shifts, factorized_shifted_matrices, LM_eig = hpro.negative_eigenvalues_of_hmatrix_pencil(A_hmatrix, B_hmatrix, threshold=threshold)
@@ -72,16 +75,18 @@ for mu, fac in zip(shifts, factorized_shifted_matrices):
     fac_err = np.linalg.norm(x - x_true) / np.linalg.norm(x_true)
     print('mu=', mu, ', fac_err=', fac_err)
 
-##
 
-mu_max = -np.min(ee)*1.0
-mu_min = 0.5
+########    hpro.make_shifted_hmatrix_inverse_interpolator    ########
+
+mu_max = -np.min(ee)*1.1
+mu_min = 1.0
+mu_spacing_factor = 50.0
 
 shifted_interpolator = hpro.make_shifted_hmatrix_inverse_interpolator(
     A_hmatrix, B_hmatrix, mu_min, mu_max, gamma=-1.0,
-    mu_spacing_factor=50.0,
+    mu_spacing_factor=mu_spacing_factor,
     deflation_dd=dd, deflation_V=V, LM_eig=LM_eig, known_mus=[-s for s in shifts],
-    known_shifted_factorizations=factorized_shifted_matrices)
+    known_shifted_factorizations=factorized_shifted_matrices, display=True)
 
 
 b = np.random.randn(N)
@@ -97,3 +102,26 @@ for mu in mus:
     print('mu=', mu, ', err=', err)
 
 plt.loglog(mus, errs)
+
+
+########    hpro.deflate_negative_eigs_then_make_shifted_hmatrix_inverse_interpolator    ########
+
+two_B_hmatrix = hpro.h_scale(B_hmatrix, 2.0, overwrite_A=False)
+
+shifted_interpolator2 = hpro.deflate_negative_eigs_then_make_shifted_hmatrix_inverse_interpolator(
+    A_hmatrix, two_B_hmatrix, 0.5*mu_min, 0.5*mu_max, mu_spacing_factor=mu_spacing_factor, display=True,
+    threshold=threshold
+)
+
+b = np.random.randn(N)
+mus = list(np.logspace(np.log10(0.5*mu_min), np.log10(0.5*mu_max), 100))
+# mus = shifted_interpolator2.known_mus
+errs = []
+for mu in mus:
+    x_true = np.linalg.solve(A_plus_dense + mu * 2.0 * B_dense, b)
+    x = shifted_interpolator2.solve_shifted_deflated_preconditioner(b, mu, display=False)
+    err = np.linalg.norm(x_true - x) / np.linalg.norm(x_true)
+    errs.append(err)
+    print('mu=', mu, ', err=', err)
+
+plt.loglog(2.0*np.array(mus), errs)